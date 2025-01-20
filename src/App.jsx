085b1561db7e98320a7e7083232567d704df2d37
Ale <ale.naengram@gmail.com>
2025-01-20 09:21:01 +0100

    añadiendo componentes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Tablero from './components/tablero/Tablero';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Tablero />
+    </div>
+  );
+}
+
+export default App;
